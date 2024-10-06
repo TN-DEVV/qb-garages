3fb92040433fd09364410862f1de2409cea16a8b
DON <93664733+TN-DEVV@users.noreply.github.com>
2024-10-06 13:08:42 +0100

    add sql file

--- player_vehicles.sql
+++ player_vehicles.sql
@@ -1,12 +1,41 @@
-CREATE TABLE IF NOT EXISTS `player_vehicles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 06, 2024 at 02:06 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `qbcoreframework_a3e8d4`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `player_vehicles`
+--
+
+CREATE TABLE `player_vehicles` (
+  `id` int(11) NOT NULL,
   `license` varchar(50) DEFAULT NULL,
-  `citizenid` varchar(50) DEFAULT NULL,
+  `citizenid` varchar(11) DEFAULT NULL,
   `vehicle` varchar(50) DEFAULT NULL,
   `hash` varchar(50) DEFAULT NULL,
-  `mods` longtext DEFAULT NULL,
-  `plate` varchar(50) NOT NULL,
-  `fakeplate` varchar(50) DEFAULT NULL,
+  `mods` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `plate` varchar(8) NOT NULL,
+  `fakeplate` varchar(8) DEFAULT NULL,
   `garage` varchar(50) DEFAULT NULL,
   `fuel` int(11) DEFAULT 100,
   `engine` float DEFAULT 1000,
@@ -19,8 +48,35 @@ CREATE TABLE IF NOT EXISTS `player_vehicles` (
   `paymentamount` int(11) NOT NULL DEFAULT 0,
   `paymentsleft` int(11) NOT NULL DEFAULT 0,
   `financetime` int(11) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`),
-  KEY `plate` (`plate`),
-  KEY `citizenid` (`citizenid`),
-  KEY `license` (`license`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `logs` longtext DEFAULT '[]',
+  `loandate` varchar(255) DEFAULT NULL,
+  `loanperiode` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `player_vehicles`
+--
+ALTER TABLE `player_vehicles`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `plate` (`plate`),
+  ADD KEY `citizenid` (`citizenid`),
+  ADD KEY `license` (`license`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `player_vehicles`
+--
+ALTER TABLE `player_vehicles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
